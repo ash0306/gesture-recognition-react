56a86ee902ceb44b61e5e1daef55d425d17752af
Aswathi <aswathi259@gmail.com>
2023-08-28 14:45:39 +0530

    Add. Added gesture recognition for in-built gestures in fingerpose

--- src/components/HandposeComponent/HandposeComponent.jsx
+++ src/components/HandposeComponent/HandposeComponent.jsx
@@ -1,13 +1,16 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import './HandposeComponent.css'
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import { drawHand } from '../utilities';
+import * as fp from 'fingerpose';
 
 function HandposeComponent() {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
+
+    const [msg,setMsg] = useState(null);
 
     const runHandpose = async () => {
         const net = await handpose.load();
@@ -34,9 +37,31 @@ function HandposeComponent() {
             canvasRef.current.width = videoWidth;
             canvasRef.current.height = videoHeight;
 
+            //make the hand detections
             const hand = await net.estimateHands(video);
             console.log(hand);
 
+            if(hand.length > 0) {
+                const GE = new fp.GestureEstimator([
+                    fp.Gestures.VictoryGesture,
+                    fp.Gestures.ThumbsUpGesture,
+                ])
+
+                const gesture = await GE.estimate(hand[0].landmarks, 8);
+                console.log(gesture);
+
+                if(gesture.gestures !== undefined && gesture.gestures.length > 0){
+                    setMsg(gesture.gestures[0].name);
+                } 
+                else{
+                    setMsg("No Gesture");
+                }
+            }
+            else{
+                setMsg("No Hand");
+            }
+
+            //draw the mesh
             const ctx = canvasRef.current.getContext("2d");
             drawHand(hand, ctx);
         }
@@ -45,9 +70,11 @@ function HandposeComponent() {
     runHandpose();
 
   return (
-    <div>
+    <div className='cam-display'>
         <Webcam ref={webcamRef}/>
         <canvas ref={canvasRef}/>
+
+        {msg && <div className='gesture-name'>{msg}</div>}
     </div>
   )
 }
